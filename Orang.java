76c6463c6b85f9060403b51db804b3550685ced5
Wina69 <wrachma627@gmail.com>
2017-03-26 15:31:32 +0700

    Add files via upload
    masih error

---
+++ Orang.java
@@ -0,0 +1,20 @@
+public class Orang{
+
+	String nama, hobi;
+	int umur;
+	void inputData (String nama, String hobi, Int umur){
+		nama = x;
+		hobi = y;
+		umur = z;
+	}
+	void cetak(){
+		System.out.println("Nama = +nama");
+		System.out.println("Hobi = +hobi");
+		System.out.println("umur = +umur");
+	}
+	public static void main(String[] args) {
+		Orang mhs = new Orang();
+		Orang.inputData();
+		mhs.cetak();
+	}
+}
