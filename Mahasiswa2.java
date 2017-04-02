e8d31de55075657cd570df9a2632561c8660bc63
Wina69 <wrachma627@gmail.com>
2017-04-02 17:23:28 +0700

    Add files via upload
    pertemuan ke-4

---
+++ Mahasiswa2.java
@@ -0,0 +1,11 @@
+public class Mahasiswa2{
+	String nama;
+	Mahasiswa2(){
+		//constructor
+		nama = ("java");
+	}
+		public static void main(String[] args) {
+			Mahasiswa2 mhs = new Mahasiswa2();
+			System.out.println(mhs.nama);
+	}
+}
