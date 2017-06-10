3414e3f313380cb19839c7ba8c9f227e3aad2ba0
Wina69 <wrachma627@gmail.com>
2017-06-11 04:34:41 +0700

    Class Encapsulation

---
+++ Animal2.java
@@ -0,0 +1,39 @@
+public class Animal2 {
+	String nama;
+	String jenis;
+	String warna;
+	String kaki;
+		
+		public void setNama(String nama) {
+			this.nama = nama;
+		}
+
+		public String getNama () {
+			return nama;
+		}
+
+		public void setJenis(String jenis) {
+			this.jenis = jenis;
+		}
+
+		public String getJenis() {
+			return jenis;
+		}
+
+		public void setWarna(String warna){
+			this.warna = warna;
+
+		}
+
+		public String getWarna() {
+			return warna;
+		}
+
+		public void setKaki(String kaki) {
+			this.kaki = kaki;
+		}
+
+		public String getKaki() {
+			return kaki;
+		}
+	}
---
+++ MainAnimal2.java
@@ -0,0 +1,16 @@
+public class MainAnimal2{
+
+	public static void main(String[] args) {
+
+		Animal2 harimau = new Animal2();
+
+		harimau.setNama("Harimau");
+		harimau.setJenis("Jenis Karnivora");
+		harimau.setWarna("warna belang coklat");
+
+		System.out.println(harimau.getNama());
+		System.out.println(harimau.getJenis());
+		System.out.println(harimau.getWarna());
+
+	}
+}
