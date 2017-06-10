01d86e8850b4a7daa1f0c942295236e56c789ee9
Wina69 <wrachma627@gmail.com>
2017-06-11 05:42:38 +0700

    Latihan class abstrack

---
+++ Wina.java
@@ -0,0 +1,10 @@
+public class Wina extends Mahasiswa3{
+
+	protected void lulus (){
+		System.out.println("Saya pengen cepat lulus, dengan cara selalu hadir di kelas dan belajar dengan baik");
+	}
+
+	protected void kuliah(){
+		System.out.println("Saya kuliah pada hari Sabtu dan Minggu");
+	}
+}
